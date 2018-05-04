e82f0c3a585231cba7dbfd08697963dc36b6e207
Fernando Terra <nandobrt@gmail.com>
2018-05-04 17:23:50 -0300

    feat: Hook up Redux to the App

---
+++ src/components/Wallet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Wallet extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Wallet balance</h2>
+      </div>
+    );
+  }
+}
+
+export default Wallet;
