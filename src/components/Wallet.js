5da241758cc18ce21862412003a365f177445bbc
Fernando Terra <nandobrt@gmail.com>
2018-05-07 12:11:33 -0300

    feat: Change Wallet and add tests to it

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class Wallet extends Component {
+export class Wallet extends Component {
   render() {
     return (
       <div>
-        <h2>Wallet balance</h2>
+        <h3 className="balance">Wallet balance: {this.props.balance}</h3>
       </div>
     );
   }
 }
 
-export default Wallet;
+export default connect(state => {balance: state}, null)(Wallet);
